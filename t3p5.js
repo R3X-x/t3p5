ac8df876043e9b9c3a16d10aa9bc1f462935b886
R3X-x <81946959+R3X-x@users.noreply.github.com>
2022-06-25 01:16:02 -0500

    Update t3p5.js

--- t3p5.js
+++ t3p5.js
@@ -7,15 +7,15 @@ ctx.rect(160,160,90,240)
 ctx.stroke()
 ctx.fill()
 
-ctx.fillStyle="grey"
-ctx.strokeStyle="black"
+ctx.fillStyle="black"
+ctx.strokeStyle="#878687"
 ctx.beginPath()
 ctx.rect(160,120,90,40)
 ctx.stroke()
 ctx.fill()
 
-ctx.strokeStyle="#4f4a4a"
-ctx.fillStyle="#fade9d"
+ctx.strokeStyle="black"
+ctx.fillStyle="#ffb34f"
 ctx.beginPath()
 ctx.moveTo(160,400)
 ctx.lineTo(200,480)
@@ -25,7 +25,7 @@ ctx.stroke()
 ctx.fill()
 
 ctx.strokeStyle="black"
-ctx.fillStyle="#4f4a4a"
+ctx.fillStyle="black"
 ctx.beginPath()
 ctx.moveTo(180,440)
 ctx.lineTo(220,440)
@@ -35,13 +35,13 @@ ctx.stroke()
 ctx.fill()
 
 ctx.strokeStyle="black"
-ctx.fillStyle="pink"
+ctx.fillStyle="#ffabff"
 ctx.beginPath()
 ctx.arc(200,120,40,1*Math.PI,2*Math.PI)
 ctx.stroke()
 ctx.fill()
 
-ctx.fillStyle="blue"
+ctx.fillStyle="black"
 ctx.strokestyle="black"
 ctx.beginpath()
 ctx.rect(80,80,90,240)
